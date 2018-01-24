7d5fb85c1ab61b2fd59ed59e26fa111ef41c03a8
DeneSach <35757271+DeneSach@users.noreply.github.com>
2018-01-24 12:14:57 +0530

    Add files via upload

---
+++ FortressTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by DABEYGUNAWARDENA on 1/24/2018.
+ */
+public class FortressTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Fortress Team!!!...");
+    }
+}
